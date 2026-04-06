b6fb78bb04a48c4e35cfb81b4ed89cf9454fa4f1
Alesya <alesyagdh@gmail.com>
2026-04-06 19:07:24 +0300

    changed solution #2

--- app/main.py
+++ app/main.py
@@ -1,12 +1,13 @@
 class Animal:
-    alive: list = []
+    alive: list[Animal] = []
 
     def __init__(self, name: str, health: int = 100,
                  hidden: bool = False) -> None:
         self.health = health
         self.name = name
         self.hidden = hidden
-        Animal.alive.append(self)
+        if self not in Animal.alive:
+            Animal.alive.append(self)
 
     def __repr__(self) -> str:
         return (f"{{Name: {self.name}, "
